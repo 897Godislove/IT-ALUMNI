f6d65a14d291cf4083bc92fdc3b254f263c81a6c
897Godislove <busayo.at.work@gmail.com>
2022-01-26 10:34:00 +0100

    Our sort of final project from our Industrial Training

---
+++ landingPage.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Landing Page</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/line-icons.css">
+  <link rel="stylesheet" href="css/owl.carousel.css">
+  <link rel="stylesheet" href="css/owl.theme.css">
+  <link rel="stylesheet" href="css/nivo-lightbox.css">
+  <link rel="stylesheet" href="css/magnific-popup.css">
+  <link rel="stylesheet" href="css/animate.css">
+  <link rel="stylesheet" href="css/menu_sideslide.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/responsive.css">
+</head>
+
+<body>
+  <div class="menu-wrap">
+    <nav class="menu navbar">
+      <div class="icon-list navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#video-area">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#services">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#features">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#portfolios">About us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#blog">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <button class="close-button" id="close-button"><i class="lnr lnr-cross"></i></button>
+  </div>
+  <!-- Header Section Start -->
+
+  <header id="video-area" data-stellar-background-ratio="0.5">
+    <div id="block" data-vide-bg="video/video"></div>
+    <div class="fixed-top">
+      <div class="container">
+        <div class="logo-menu">
+          <a href="index.html" class="logo"><span class="lnr lnr-diamond"></span> ICT ALUMNI</a>
+          <button class="menu-button" id="open-button"><i class="lnr lnr-menu"></i></button>
+        </div>
+      </div>
+    </div>
+    <div class="overlay overlay-2"></div>
+    <div class="container">
+      <div class="row justify-content-md-center">
+        <div class="col-md-10">
+          <div class="contents text-center">
+            <h1 class="wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="0.3s">We Always Stick Together To Build Eachother up</h1>
+            <p class="lead  wow fadeIn" data-wow-duration="1000ms" data-wow-delay="400ms">This Is All About What You Have To Offer And All The Available Opportunities Within Your Reach. Remember, No One Is An Island.</p>
+            <a href="#" class="btn btn-common wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="400ms">Signup</a>
+            <a href="#" class="btn btn-common wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="400ms">Login</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- Header Section End -->
+
+  <!-- Services Section Start -->
+  <section id="services" class="section">
+    <div class="container">
+      <div class="section-header">
+        <h2 class="section-title wow fadeIn" data-wow-duration="1000ms" data-wow-delay="0.3s">Our <span>Services</span></h2>
+        <hr class="lines wow zoomIn" data-wow-delay="0.3s">
+        <p class="section-subtitle wow fadeIn" data-wow-duration="1000ms" data-wow-delay="0.3s">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy <br> nibh euismod tincidunt ut laoreet dolore magna.</p>
+      </div>
+      <div class="row">
+        <div class="col-md-4 col-sm-6">
+          <div class="item-boxes wow fadeInDown" data-wow-delay="0.2s">
+            <div class="icon">
+              <i class="lnr lnr-pencil"></i>
+            </div>
+            <h4>Content Writing</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.</p>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="item-boxes wow fadeInDown" data-wow-delay="0.4s">
+            <div class="icon">
+              <i class="lnr lnr-cog"></i>
+            </div>
+            <h4>Web Development</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.</p>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="item-boxes wow fadeInDown" data-wow-delay="0.6s">
+            <div class="icon">
+              <i class="lnr lnr-chart-bars"></i>
+            </div>
+            <h4>Graphic Design</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.</p>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="item-boxes wow fadeInDown" data-wow-delay="0.8s">
+            <div class="icon">
+              <i class="lnr lnr-layers"></i>
+            </div>
+            <h4>UI/UX Design</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.</p>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="item-boxes wow fadeInDown" data-wow-delay="1s">
+            <div class="icon">
+              <i class="lnr lnr-tablet"></i>
+            </div>
+            <h4>App Development</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.</p>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="item-boxes wow fadeInDown" data-wow-delay="1.2s">
+            <div class="icon">
+              <i class="lnr lnr-briefcase"></i>
+            </div>
+            <h4>Digital Marketing</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Services Section End -->
+
+
+
+
+  <!-- jQuery first, then Tether, then Bootstrap JS. -->
+  <script src="js/jquery-min.js"></script>
+  <script src="js/tether.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/classie.js"></script>
+  <script src="js/mixitup.min.js"></script>
+  <script src="js/nivo-lightbox.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.nav.js"></script>
+  <script src="js/smooth-scroll.js"></script>
+  <script src="js/smooth-on-scroll.js"></script>
+  <script src="js/wow.js"></script>
+  <script src="js/menu.js"></script>
+  <script src="js/jquery.vide.js"></script>
+  <script src="js/jquery.counterup.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/waypoints.min.js"></script>
+  <script src="js/form-validator.min.js"></script>
+  <script src="js/contact-form-script.js"></script>
+  <script src="js/main.js"></script>
+
+</body>
+
+</html>
